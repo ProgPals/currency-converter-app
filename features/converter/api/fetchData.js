1f6d8faf41fc57e062f37893c7b796a1fe33f6dd
soumesh2002 <soumesh6622@gmail.com>
2023-02-10 23:34:32 +0530

    correction: added '=' after apikey

--- features/converter/api/fetchData.js
+++ features/converter/api/fetchData.js
@@ -2,14 +2,14 @@ import { axios } from "../../../lib/axios";
 
 export const fetchRates = async (currencyOne) => {
   const { data } = await axios.get(
-    `/lastest?base=${currencyOne}&apikey${import.meta.env.VITE_API_KEY}`
+    `/lastest?base=${currencyOne}&apikey=${import.meta.env.VITE_API_KEY}`
   );
   return data;
 };
 
 export const fetchSymbols = async () => {
   const { data } = await axios.get(
-    `/symbols?apikey${import.meta.env.VITE_API_KEY}`
+    `/symbols?apikey=${import.meta.env.VITE_API_KEY}`
   );
   return data;
 };
