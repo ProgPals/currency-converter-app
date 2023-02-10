9186fedc9c4baa3c95392e04a2279c3212580874
soumesh2002 <soumesh6622@gmail.com>
2023-02-11 00:02:34 +0530

    error ⚠️

--- features/common/hooks/useCurrency.js
+++ features/common/hooks/useCurrency.js
@@ -1,9 +1,33 @@
+import { useQueries } from "@tanstack/react-query";
 import { useState } from "react";
+import {
+  fetchDate,
+  fetchRates,
+  fetchSymbols,
+} from "../../converter/api/fetchData";
 
 export const useCurrency = () => {
   const [amount, setAmount] = useState(25);
   const [currencyOne, setCurrencyOne] = useState("INR");
   const [currencyTwo, setCurrencyTwo] = useState("EUR");
+
+  const [ratesData, symbolsData, date] = useQueries({
+    queries: [
+      {
+        date: () => fetchDate("YYYY-MM-DD"),
+      },
+      {
+        queryKey: ["rates", currencyOne],
+        queryFn: () => fetchRates(currencyOne),
+        staleTime: Infinity,
+      },
+      {
+        queryKey: ["symbols"],
+        queryFn: () => fetchSymbols,
+        staleTime: Infinity,
+      },
+    ],
+  });
 
   return { amount, currencyOne, currencyTwo };
 };
