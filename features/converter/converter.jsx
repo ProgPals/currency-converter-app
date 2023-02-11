0e815d24f4a2e180841c02342c48d0c0f9766a7e
soumesh2002 <soumesh6622@gmail.com>
2023-02-12 00:38:17 +0530

    added some chakra-UI elements

--- features/converter/converter.jsx
+++ features/converter/converter.jsx
@@ -1,7 +1,43 @@
+import { Spinner, Text } from "@chakra-ui/react";
 import { useCurrency } from "../common/hooks/useCurrency";
 
 const Converter = () => {
-  const { amount, currencyOne, currencyTwo } = useCurrency();
+  const {
+    isLoading,
+    isError,
+    amount,
+    setAmount,
+    currencyOne,
+    currencyTwo,
+    setCurrencyOne,
+    setCurrencyTwo,
+    currencyList,
+    covertedAmount,
+    ratesData,
+    symbolsData,
+    date,
+    time,
+  } = useCurrency();
+
+  if (isError)
+    return (
+      <Text fontWeight="bold" fontSize="3xl" color="white" my="10">
+        Something has gone terribly wrong
+      </Text>
+    );
+
+  if (isLoading)
+    return (
+      <Spinner
+        margin="auto 0"
+        size="xl"
+        thickness="4px"
+        speed="0.65s"
+        color="purple.500"
+        emptyColor="purple.200"
+      />
+    );
+
   return (
     <>
       <>{amount}</>
