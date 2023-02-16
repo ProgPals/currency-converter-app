6247a618cb45fd9d15a91109dcacfd54c8bd7e73
soumesh2002 <soumesh6622@gmail.com>
2023-02-16 18:59:10 +0530

    Commit made from VS Code Azure Static Web Apps

--- features/converter/api/fetchData.js
+++ features/converter/api/fetchData.js
@@ -2,7 +2,7 @@ import { axios } from "../../../lib/axios";
 
 export const fetchRates = async (currencyOne) => {
   const { data } = await axios.get(
-    `/lastest?base=${currencyOne}&apikey=${import.meta.env.VITE_API_KEY}`
+    `/latest?base=${currencyOne}&apikey=${import.meta.env.VITE_API_KEY}`
   );
   return data;
 };
